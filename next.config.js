fe60ee430d972c6352e5323d3c962e4b3bde432e
Hahyeon Lee <im.madylin@gmail.com>
2022-10-20 09:32:23 +0900

    modify error

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: './',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {unoptimized: true,}
 }
 

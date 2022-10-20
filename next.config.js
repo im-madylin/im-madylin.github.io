e69c9647770edb898bd7e2ed2b56415c32f0844b
Hahyeon Lee <im.madylin@gmail.com>
2022-10-20 10:11:50 +0900

    modify to apply css

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ''
+let assetPrefix = undefined
 let basePath = undefined
 
 if (isGithubActions) {
   // trim off `<owner>/`
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-  assetPrefix = `/${repo}/`
+  assetPrefix = './'
   basePath = `/${repo}`
 }
 

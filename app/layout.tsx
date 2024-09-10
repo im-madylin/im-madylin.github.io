134771fd06e5f828dae938ffa7fd56d14e0b1c77
im-madylin <im.madylin@gmail.com>
2024-09-10 15:57:53 +0900

    feat: 타이틀, 이미지 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${noto_sans.className} antialiased`}>{children}</body>
+    <html lang="ko">
+      <body className={`${noto_sans.className}`}>{children}</body>
     </html>
   );
 }
---
+++ components/Cover.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+const Cover: React.FC = () => {
+  return (
+    <section
+      id="cover"
+      className="flex h-screen w-full flex-col items-center justify-center"
+    >
+      <div className="flex h-full w-full flex-col items-center justify-center gap-14">
+        <h1 className="text-center text-6xl font-bold">
+          프론트엔드 개발자 이하현 입니다.
+        </h1>
+        <p className="mt-4 text-center text-3xl">
+          IT기업에서 사용자 경험을 향상시키기 위해 노력한 경험이 있습니다.{" "}
+          <br />
+          소통과 협력의 가치를 깊이 인지하고 있습니다.
+        </p>
+      </div>
+      <div className="relative flex h-full w-full">
+        <Image
+          src="/images/profile.png"
+          fill
+          sizes="100vw"
+          className="object-cover"
+          alt="profile"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Cover;

1ad1dd7f00ace8e2088da30ee20b0345ff787d52
im-madylin <im.madylin@gmail.com>
2024-09-13 14:31:31 +0900

    feat: introduction 추가

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { introContents } from "../contents/aboutMe";
 
 const AboutMe: React.FC = () => {
   return (
@@ -17,11 +18,34 @@ const AboutMe: React.FC = () => {
         className="h-auto w-full object-cover"
         alt="html code"
       />
-      <div>
-        <h2>Introduction</h2>
-        <div>
-          <h3>질문</h3>
-          <p>답변</p>
+      <div className="flex w-full flex-col items-center justify-center gap-20">
+        <h2 className="text-center text-7xl font-bold">Introduction</h2>
+        <div className="flex w-full flex-col items-center justify-center gap-16">
+          {introContents.map((content, index) => {
+            const regex = new RegExp(`(${content.highlights.join("|")})`, "g");
+            const answer = content.answer.split(regex);
+            return (
+              <div
+                key={index}
+                className="flex w-full flex-col items-center justify-center gap-8 text-left"
+              >
+                <h3 className="w-full text-3xl font-bold">
+                  Q. {content.question}
+                </h3>
+                <p className="w-full text-xl font-semibold text-gray-500">
+                  {answer.map((text, index) =>
+                    content.highlights.includes(text) ? (
+                      <span key={index} className="text-black">
+                        {text}
+                      </span>
+                    ) : (
+                      <span key={index}>{text}</span>
+                    ),
+                  )}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
       <div>

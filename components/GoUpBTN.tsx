d64cd8a78d680fa560fef1c183d0bc8c8dc68f48
im-madylin <im.madylin@gmail.com>
2024-09-13 17:38:50 +0900

    feat: 위로가기 버튼 기능 추가

--- components/GoUpBTN.tsx
+++ components/GoUpBTN.tsx
@@ -1,10 +1,19 @@
+"use client";
+
 import { FaArrowUp } from "react-icons/fa";
 
 const GoUpBTN: React.FC = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <div className="bg-appleGray-100/70 fixed bottom-4 right-5 z-10 flex h-10 w-10 items-center justify-center rounded-3xl">
-      <FaArrowUp className="text-appleGray-500 text-2xl" />
-    </div>
+    <button
+      onClick={scrollToTop}
+      className="fixed bottom-4 right-5 z-10 flex h-10 w-10 items-center justify-center rounded-3xl bg-appleGray-100/70"
+    >
+      <FaArrowUp className="text-2xl text-appleGray-500" />
+    </button>
   );
 };
 

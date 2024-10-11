56b09baf72dfc96be64ef6329a6b357450d56909
im-madylin <im.madylin@gmail.com>
2024-10-11 17:09:03 +0900

    feat: FAB 애니메이션 및 링크 기능 추가

--- components/FloatingActionBTN.tsx
+++ components/FloatingActionBTN.tsx
@@ -1,19 +1,68 @@
-const NavBar: React.FC = () => {
+"use client";
+
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { FaGithub } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { IoNewspaper } from "react-icons/io5";
+
+const FloatingActionBTN: React.FC = () => {
+  const [isFABHovered, setIsFABHovered] = useState(false);
+
+  const btnVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 2,
+        duration: 1,
+        ease: "easeInOut",
+      },
+    },
+  };
+
   const menu = [
-    { name: "Github", href: "#" },
-    { name: "Email", href: "#" },
-    { name: "Blog", href: "#" },
+    {
+      name: "Github",
+      tag: <FaGithub />,
+      href: "https://github.com/im-madylin",
+    },
+    {
+      name: "Blog",
+      tag: <IoNewspaper />,
+      href: "https://im-madylin.tistory.com",
+    },
+    { name: "Email", tag: <IoIosMail />, href: "mailto:im.madylin@gmail.com" },
   ];
   return (
-    <div className="bg-appleGray-100/70 fixed bottom-16 right-5 z-10 flex w-10 flex-col items-center justify-center gap-2 rounded-3xl p-4">
-      {menu.map((item, index) => (
-        <div
-          key={index}
-          className="bg-appleGray-500 h-3 w-3 rounded-full"
-        ></div>
-      ))}
-    </div>
+    <motion.button
+      initial="hidden"
+      animate="visible"
+      variants={btnVariants}
+      onMouseEnter={() => setIsFABHovered(true)}
+      onMouseLeave={() => setIsFABHovered(false)}
+      className="fixed bottom-16 right-5 z-10 flex w-10 flex-col items-center justify-center gap-2 rounded-3xl bg-appleGray-100/70 p-4"
+    >
+      {menu.map((item, index) => {
+        return isFABHovered ? (
+          <a
+            key={index}
+            href={item.href}
+            target="_blank"
+            rel="noreferrer"
+            className="flex h-fit transform items-center justify-center text-2xl text-gray-800 transition-all duration-1000 ease-in-out hover:scale-125"
+          >
+            {item.tag}
+          </a>
+        ) : (
+          <div
+            key={index}
+            className="h-3 w-3 rounded-full bg-appleGray-500 transition-all duration-1000 ease-in-out"
+          ></div>
+        );
+      })}
+    </motion.button>
   );
 };
 
-export default NavBar;
+export default FloatingActionBTN;
--- components/GoUpBTN.tsx
+++ components/GoUpBTN.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "framer-motion";
 import { FaArrowUp } from "react-icons/fa";
 
 const GoUpBTN: React.FC = () => {
@@ -7,13 +8,28 @@ const GoUpBTN: React.FC = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
+  const btnVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 2,
+        duration: 1,
+        ease: "easeInOut",
+      },
+    },
+  };
+
   return (
-    <button
+    <motion.button
       onClick={scrollToTop}
+      initial="hidden"
+      animate="visible"
+      variants={btnVariants}
       className="fixed bottom-4 right-5 z-10 flex h-10 w-10 items-center justify-center rounded-3xl bg-appleGray-100/70"
     >
       <FaArrowUp className="text-2xl text-appleGray-500" />
-    </button>
+    </motion.button>
   );
 };
 

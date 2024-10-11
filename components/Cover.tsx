e34faa093f6e5df7979de269f719e36fa33db4c4
im-madylin <im.madylin@gmail.com>
2024-10-11 11:32:25 +0900

    feat: 표지 애니메이션 적용

--- components/Cover.tsx
+++ components/Cover.tsx
@@ -1,34 +1,119 @@
+"use client";
+
+import { motion } from "framer-motion";
 import Image from "next/image";
 import { LiaAngleDoubleDownSolid } from "react-icons/lia";
 
 const Cover: React.FC = () => {
+  const imageVariants = {
+    hidden: { scale: 3, opacity: 0 },
+    visible: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        duration: 2,
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  const h1Variants = {
+    hidden: { opacity: 0, y: -50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 1,
+        duration: 1,
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  const textVariants = {
+    hidden: { opacity: 0, y: -50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 1.5,
+        duration: 1,
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  const iconAndDateVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 2,
+        duration: 1,
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  const bounceAnimation = {
+    visible: {
+      y: [0, -10, 0], // 0에서 -10px 위로 올라갔다가 다시 내려옴
+      transition: {
+        repeat: Infinity, // 무한 반복
+        repeatType: "loop", // 루프 형태로 반복
+        duration: 0.9, // 한 번의 애니메이션이 0.6초 동안 실행
+        ease: "easeInOut",
+      },
+    },
+  };
+
   return (
     <section
       id="cover"
       className="flex h-screen w-full flex-col items-center justify-center"
     >
       <div className="flex h-full w-full flex-col items-center justify-center gap-14">
-        <h1 className="text-center text-6xl font-bold">
-          프론트엔드 개발자 이하현 입니다.
-        </h1>
-        <p className="mt-4 text-center text-3xl">
-          IT기업에서 사용자 경험을 향상시키기 위해 노력한 경험이 있습니다.{" "}
-          <br />
-          소통과 협력의 가치를 깊이 인지하고 있습니다.
-        </p>
+        <motion.div initial="hidden" animate="visible" variants={h1Variants}>
+          <h1 className="text-center text-6xl font-bold">
+            프론트엔드 개발자 이하현 입니다.
+          </h1>
+        </motion.div>
+        <motion.div initial="hidden" animate="visible" variants={textVariants}>
+          <p className="mt-4 text-center text-3xl">
+            IT기업에서 사용자 경험을 향상시키기 위해 노력한 경험이 있습니다.{" "}
+            <br />
+            소통과 협력의 가치를 깊이 인지하고 있습니다.
+          </p>
+        </motion.div>
       </div>
       <div className="relative h-full w-full">
-        <Image
-          src="/images/profile.webp"
-          fill
-          sizes="100vw"
-          className="object-cover"
-          alt="profile"
-        />
-        <p className="absolute bottom-3 left-4 z-10 text-base text-gray-300">
-          Last Update 2024.09.13
-        </p>
-        <LiaAngleDoubleDownSolid className="absolute bottom-1 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 transform justify-center text-5xl text-white" />
+        <motion.div
+          className="relative h-full w-full"
+          initial="hidden"
+          animate="visible"
+          variants={imageVariants}
+        >
+          <Image
+            src="/images/profile.webp"
+            fill
+            sizes="100vw"
+            className="object-cover"
+            alt="profile"
+          />
+        </motion.div>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={iconAndDateVariants}
+        >
+          <p className="absolute bottom-3 left-4 z-10 text-base text-gray-300">
+            Last Update 2024.09.13
+          </p>
+          <motion.div animate="visible" variants={bounceAnimation}>
+            <LiaAngleDoubleDownSolid className="absolute bottom-1 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 transform justify-center text-5xl text-white" />
+          </motion.div>
+        </motion.div>
       </div>
     </section>
   );

b3650b36592f4de8cc92e58a51b7be8049f13a70
im-madylin <im.madylin@gmail.com>
2024-10-25 22:54:36 +0900

    fix: 타이핑 글자 수 조정

--- components/Cover/TypingText.tsx
+++ components/Cover/TypingText.tsx
@@ -1,9 +1,7 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 
-// 타이핑 애니메이션 구현 컴포넌트
 const TypingText: React.FC = () => {
   const [text, setText] = useState<string[]>([""]);
-  const [isTyping, setIsTyping] = useState(true);
   const words = useMemo(
     () => [
       "IT기업에서 사용자 경험을 향상시키기 위해 노력한 경험이 있습니다.",
@@ -13,11 +11,12 @@ const TypingText: React.FC = () => {
   );
 
   const START_DELAY = 2000;
-  const LETTER_TYPE_DELAY = 300;
+  const LETTER_TYPE_DELAY = 100;
   const WORD_STAY_DELAY = 500;
 
   const wordIndex = useRef(0);
   const letterIndex = useRef(0);
+  const typingTimeout = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     const typeLetter = () => {
@@ -30,30 +29,28 @@ const TypingText: React.FC = () => {
 
       letterIndex.current += 1;
 
-      // 한 줄 타이핑 후 잠시 대기
       if (letterIndex.current === word.length) {
-        setIsTyping(false);
-        setTimeout(() => {
+        clearTimeout(typingTimeout.current!);
+        typingTimeout.current = setTimeout(() => {
           if (wordIndex.current < words.length - 1) {
             wordIndex.current += 1;
             letterIndex.current = 0;
-            setIsTyping(true);
-            setTimeout(typeLetter, LETTER_TYPE_DELAY);
+            typingTimeout.current = setTimeout(typeLetter, LETTER_TYPE_DELAY);
           }
         }, WORD_STAY_DELAY);
         return;
       }
-      setTimeout(typeLetter, LETTER_TYPE_DELAY);
+      typingTimeout.current = setTimeout(typeLetter, LETTER_TYPE_DELAY);
     };
 
-    const startTyping = () => {
-      setTimeout(typeLetter, START_DELAY);
-    };
+    typingTimeout.current = setTimeout(typeLetter, START_DELAY);
 
-    if (isTyping) {
-      startTyping();
-    }
-  }, [isTyping, words]);
+    return () => {
+      if (typingTimeout.current) {
+        clearTimeout(typingTimeout.current);
+      }
+    };
+  }, [words]);
 
   return (
     <div className="typing-text text-center text-3xl">
